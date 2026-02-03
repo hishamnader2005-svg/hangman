0605e9fc09a1c9d2a3960f48133ce99fc6b60fa1
etsh2005 <c24377113@mytudublin.ie>
2026-02-03 19:29:28 +0000

    init

--- src/hangman.java
+++ src/hangman.java
@@ -2,21 +2,30 @@ import java.util.Scanner;
 import java.util.HashSet;
 import java.util.Random;
 
+public class Hangman {
 
-public class hangman {
     public int lives;
-    public int  faults;
-    HashSet<String> word ;
+    public int faults;
+
+    String secretWord;
+    HashSet<Character> guessedLetters;
+
     String[] words = {
-        "java",
-        "computer",
-        "random",
+            "java",
+            "computer",
+            "random"
     };
+
     Random randword = new Random();
 
-    public hangman(int lives , int faults ){
+    public Hangman(int lives, int faults) {
         this.lives = lives;
         this.faults = faults;
 
+        // pick random word
+        secretWord = words[randword.nextInt(words.length)];
+
+        // initialize HashSet
+        guessedLetters = new HashSet<>();
     }
 }
